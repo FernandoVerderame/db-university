458fd7532b28192a0395c2756317289956a896b8
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-03-01 15:48:14 +0100

    exercise number four

--- solutions.sql
+++ solutions.sql
@@ -12,3 +12,9 @@ WHERE `cfu` > 10;
 SELECT * 
 FROM `students`
 WHERE 2024 - YEAR(`date_of_birth`) > 30;
+
+-- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+SELECT * 
+FROM `courses`
+WHERE `period` = 'I semestre'
+AND `year` = 1;
