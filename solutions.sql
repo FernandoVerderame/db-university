c0256e5f7fbc1e191290bc049614d4ad2c39ef4c
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-03-04 13:00:24 +0100

    fixed exercise number eight

--- solutions.sql
+++ solutions.sql
@@ -36,26 +36,25 @@ FROM `departments`;
 
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
-SELECT * 
-FROM `teachers`
-WHERE `phone` IS null;
+SELECT COUNT(`phone`) AS 'teachers_without_phone'
+FROM `teachers`;
 
 -- 9. Contare quanti iscritti ci sono stati ogni anno
-SELECT COUNT(*) as `total_students`, YEAR(`enrolment_date`) as `enrolment_year` 
+SELECT COUNT(*) AS `total_students`, YEAR(`enrolment_date`) AS `enrolment_year` 
 FROM `students` 
 GROUP BY `enrolment_year`;
 
 -- 10. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
-SELECT COUNT(*) as `total_teachers`, `office_address`
+SELECT COUNT(*) AS `total_teachers`, `office_address`
 FROM `teachers`
 GROUP BY `office_address`;
 
 -- 11. Calcolare la media dei voti di ogni appello d'esame
-SELECT ROUND(AVG(`vote`), 0) as `average_vote`, `exam_id`  
+SELECT ROUND(AVG(`vote`), 0) AS `average_vote`, `exam_id`  
 FROM `exam_student`
 GROUP BY `exam_id`;
 
 --12. Contare quanti corsi di laurea ci sono per ogni dipartimento
-SELECT COUNT(*) as `total_degrees`, `department_id` 
+SELECT COUNT(*) AS `total_degrees`, `department_id` 
 FROM `degrees`
 GROUP BY `department_id`;
